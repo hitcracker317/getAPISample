142b9785e406770ec21853b8bf69bc9cb4db2fc9
koseki <dainaso97@gmailcom>
2015-10-22 23:49:38 +0900

    起動時だけAPIを叩くように修正

--- getAPISample/ViewController.swift
+++ getAPISample/ViewController.swift
@@ -10,8 +10,10 @@ import UIKit
 
 class ViewController: UIViewController ,UITextFieldDelegate,UIPickerViewDelegate/*,UIPickerViewDataSource*/{
 
-    var musicIndex:Int = 0 //曲のインデックス番号！
+    var musicIndex:Int = 0 //曲のインデックス番号
     var musicCount:Int = 0 //取得した曲数
+    var musicArray:NSArray = [] //API叩いて取得した曲の情報(NSDictionary)を格納する配列
+    
     var keyBoardRect:CGRect = CGRectMake(0, 0, 0, 0) //キーボードのRect
     
     @IBOutlet weak var numberTextField: UITextField!
@@ -46,62 +48,47 @@ class ViewController: UIViewController ,UITextFieldDelegate,UIPickerViewDelegate
         
         numberTextField.inputAccessoryView = myToolBar
         
-        self.setMusicInfo()
+        self.getAPI()
+    }
+    
+    func getAPI(){
+        //iTunesのAPIを叩くメソッド
+        
+        var urlString = "https://itunes.apple.com/search?term=beatles&country=JP&lang=ja_jp&media=music" //API(url)の文字列を生成する
+        var url = NSURL(string: urlString) //URLの文字列をURL型に変換
+        var data = NSData(contentsOfURL: url!) //URLをData型に変換
+        let jsonDict = NSJSONSerialization.JSONObjectWithData(data!, options: nil, error: nil) as! NSDictionary //jsonに変換
+        println(jsonDict)
+        
+        //iTunes APIから取得した曲数
+        var musicCountString:NSNumber = jsonDict["resultCount"] as! NSNumber
+        musicCount = Int(musicCountString) //NSNumberをIntに変換
+        println("取得した曲数\(musicCount)")
+        
+        //曲の一覧情報を取得
+        musicArray = jsonDict["results"] as! NSArray
+        
+        setMusicInfo()
     }
     
     func setMusicInfo(){
-        //iTunesのAPIを叩くメソッド
-        
-        //API(url)の文字列を生成する
-        var urlString = "https://itunes.apple.com/search?term=beatles&country=JP&lang=ja_jp&media=music"
-        
-        //URLの文字列をURL型に変換
-        var url = NSURL(string: urlString)
-        
-        //URLをData型に変換
-        var data = NSData(contentsOfURL: url!)
-        
-        //jsonに変換
-        let jsonDict = NSJSONSerialization.JSONObjectWithData(data!, options: nil, error: nil) as! NSDictionary
-        println(jsonDict)
-        
-        //iTunes APIから取得した曲集
-        var musicCountString:NSNumber = jsonDict["resultCount"] as! NSNumber
-        musicCount = Int(musicCountString) //NSNumberをIntに変換
-        
-        println("ヒットした曲数\(musicCount)")
-        
-        //ヒットした曲の名前を取得
-        var musicArray = jsonDict["results"] as! NSArray
-        
         var trackName = musicArray[musicIndex]["trackName"] as! String
+        self.trackNameLabel.text = trackName //Labelに曲名を表示
         println("曲名:\(trackName)")
         
         //アートワークを取得(取得した値は画像のurlの文字列)
         //URLからImageを作成しImageViewに表示する
         //urlを文字列で生成
-        var artworkURL = musicArray[musicIndex]["artworkUrl100"] as! String
-        println(artworkURL)
-        
-        //url型に変換
-        var imageURL = NSURL(string: artworkURL)
-        
-        //data型に変換
-        var imageData = NSData(contentsOfURL: imageURL!)
-        
-        //image型に変換
-        var image = UIImage(data: imageData!)
-        
-        //ImageViewに表示
-        self.imageView.image = image
-        
-        //Labelにタイトルを表示
-        self.trackNameLabel.text = trackName
+        var artworkURL = musicArray[musicIndex]["artworkUrl100"] as! String //urlを文字列で取得
+        var imageURL = NSURL(string: artworkURL) //stringをurl型に変換
+        var imageData = NSData(contentsOfURL: imageURL!) //data型に変換
+        var image = UIImage(data: imageData!) //image型に変換
+        self.imageView.image = image //ImageViewに表示
         
         //ラベルにx/x曲目の文字列を表示
         var countString = "\(musicIndex + 1)/\(musicCount) 曲目"
         self.countLabel.text = countString
-        
+
     }
     
     func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
